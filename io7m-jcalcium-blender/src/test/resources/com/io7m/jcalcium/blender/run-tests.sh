500b41e95acfa65bb33d135fd7a3a554199705cd
Mark Raynsford <code@io7m.com>
2017-01-31 20:30:31 +0000

    Remove unused variable, reduce scope of IFS

--- io7m-jcalcium-blender/src/test/resources/com/io7m/jcalcium/blender/run-tests.sh
+++ io7m-jcalcium-blender/src/test/resources/com/io7m/jcalcium/blender/run-tests.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-OLD_IFS="${IFS}"
-IFS="
-"
-
 TEST_BLENDER=`which blender` || exit 1
 TEST_TMPDIR=`mktemp -d --tmpdir calcium-blender-XXXXXXXX` || exit 1
 TEST_COUNT=0
@@ -13,6 +9,9 @@ printf "Running test suite on %s\n" `date +%Y-%m-%dT%H:%M:%S%z || exit 1` || exi
 printf "Logging to ${TEST_TMPDIR}\n" || exit 1
 printf "blender: %s\n" "${TEST_BLENDER}"
 echo "--------------------------------------------------------------------------------"
+
+IFS="
+"
 
 for line in `cat run-test-suite.txt || exit 1`
 do
